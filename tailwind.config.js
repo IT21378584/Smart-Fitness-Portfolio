e23a9a6fb3d895bb9661cc82763e0253860405bb
Sahani Kulathilaka <99276642+IT21378584@users.noreply.github.com>
2025-05-27 18:20:36 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {content: [
+  './index.html',
+  './src/**/*.{js,ts,jsx,tsx}'
+],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          400: '#3b82f6',
+          500: '#3b82f6',
+          600: '#2563eb',
+          900: '#1e3a8a',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
